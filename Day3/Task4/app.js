59b286012c9522fedf070015e294b6c841756973
Prabesh KC <kcprabase@gmail.com>
2022-11-02 18:31:51 -0500

    Added Task 4 files

---
+++ Day3/Task4/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/:first", (req, res) => {
+    const second = parseFloat(req.query.second);
+    const first = parseFloat(req.params.first);
+    if (second == 0) {
+        res.status(404).send("cannot divide by zero");
+    } else {
+        res.status(200).send((first / second).toString());
+    }
+});
+
+app.listen(3000, () => {
+    console.log("App is listening to port ", 3000);
+});
